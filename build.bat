d8b12072706ad1e4d636a4d2c59ed933428af77a
simon <49937999+ck2739046@users.noreply.github.com>
2026-09-01 16:11:17 +0800

    [*] build.bat 构建成功后自动复制

--- build.bat
+++ build.bat
@@ -1,15 +1,27 @@
 @echo off
 setlocal
 
+set "PUBLISH_DIR=%~dp0bin\Release\net10.0\win-x64\publish"
+set "TARGET_DIR=C:\git\aaa-HachimiDX\src\resources\majdatax"
+
 pushd "%~dp0"
 dotnet publish "MajdataEdit-Neo.csproj" --configuration Release --runtime win-x64 --nologo
-set "EXIT_CODE=%ERRORLEVEL%"
 popd
 
-if "%EXIT_CODE%"!="0" (
-    echo.
-    echo Release publish failed with exit code %EXIT_CODE%.
-    pause
+if errorlevel 1 (
+    echo ---
+    echo Failed to build the project.
+	pause
+	exit /b 1
 )
 
-exit /b %EXIT_CODE%
+REM retry 10 times with 1 second wait
+robocopy "%PUBLISH_DIR%" "%TARGET_DIR%" /E /R:10 /W:1
+if errorlevel 8 (
+    echo ---
+	echo Failed to copy files.
+	pause
+	exit /b 1
+)
+
+endlocal
